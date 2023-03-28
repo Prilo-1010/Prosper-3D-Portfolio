e2759d229a45b81560dd93f39b7dc38db0b606e8
Prosper Lolo <loloprosper17@gmail.com>
2023-03-28 15:38:29 +0200

    well

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,8 +5,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import Loader from "../Loader";
 
 const Computers = ({isMobile}) => {
-  const computer = useGLTF("/src/public/desktop_pc/desktop.gltf");
-
+  // const computer = useGLTF("./desktop_pc/desktop.gltf");
+  const computer = useGLTF("../public/desktop_pc/desktop.gltf")
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
